9f42b106a919387ef682105911a57c5bb18c34cb
Sinisa Abramovic <sinisa.abramovich@gmail.com>
2018-10-28 13:07:49 +0100

    Added SimpleMove.cs for object move using GameManager (Singleton) level property

--- Creational Patterns/Sigleton Pattern/Assets/Scripts/GameManager.cs
+++ Creational Patterns/Sigleton Pattern/Assets/Scripts/GameManager.cs
@@ -2,10 +2,32 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+// Singleton pattern to use global 
+// So that we not need multiple instance of the same 
 public class GameManager : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
+    public int level = 1;
+
+    public static GameManager _instance;
+
+    public static GameManager Instance{
+        get { return _instance; }
+    }
+
+    private void Awake()
+    {
+        if(_instance == null){
+
+            // if we dont have instance 
+            _instance = this;
+        }
+
+        // for example if we wont to save data on load
+        DontDestroyOnLoad(this);
+    }
+
+    // Use this for initialization
+    void Start () {
 		
 	}
 	
---
+++ Creational Patterns/Sigleton Pattern/Assets/Scripts/SimpleMover.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SimpleMover : MonoBehaviour {
+
+    private int _speed;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Move(GameManager.Instance.level);
+	}
+
+    public void Move(int speed)
+    {
+        transform.Translate(Vector3.forward * speed * Time.deltaTime);
+    }
+}
